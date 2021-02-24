fdcbd51d6be18b6254471686b74cf88d440ec382
Your Name <you@example.com>
2021-02-24 13:05:03 +0100

    Alakul

--- StarWars.py
+++ StarWars.py
@@ -2,46 +2,55 @@ import turtle
 import random
 import time
 
-
 def fel():
-    ypozicio=urhajo.ycor()
-    ypozicio+=10
-    urhajo.sety(ypozicio)
+    ypozizcio= urhajo.ycor()
+    ypozizcio += 10
+    urhajo.sety(ypozizcio)
+
 
 def le():
-    ypozicio=urhajo.ycor()
-    ypozicio-=10
-    urhajo.sety(ypozicio)
+    ypozizcio= urhajo.ycor()
+    ypozizcio -= 10
+    urhajo.sety(ypozizcio)
+
 
 def jobbra():
-    xpozicio=urhajo.xcor()
-    xpozicio+=10
-    urhajo.setx(xpozicio)
+    xpozizcio= urhajo.xcor()
+    xpozizcio += 10
+    urhajo.setx(xpozizcio)
+
 
 def balra():
-    xpozicio=urhajo.xcor()
-    xpozicio-=10
-    urhajo.setx(xpozicio)
-
+    xpozizcio= urhajo.xcor()
+    xpozizcio -= 10
+    urhajo.setx(xpozizcio)
 
 space = turtle.Screen()
-space.addshape("sprite.gif")
-space.tracer(0)
 space.setup(width=800, height=600)
 space.bgpic("hatter.png")
+space.addshape("sprite.gif")
+space.tracer(0)
 space.listen()
 space.onkey(balra,"Left")
 space.onkey(jobbra,"Right")
 space.onkey(fel,"Up")
 space.onkey(le,"Down")
 
+
 urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
+urhajo.penup()
 
 while True:
 
     space.update()
     time.sleep(0.2)
 
-    if urhajo.ycor() > 300:
+    if urhajo.ycor()>300:
         urhajo.sety(-300)
+    if urhajo.xcor()>400:
+        urhajo.setx(-400)
+    if urhajo.xcor()<-400:
+        urhajo.setx(400)
+    if urhajo.ycor()<-300:
+        urhajo.sety(300)          
