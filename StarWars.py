33c03c35bac9ed2fb999b5d59123c58466fc77bd
Your Name <you@example.com>
2021-03-03 12:40:01 +0100

    Meteor Mozog!

--- StarWars.py
+++ StarWars.py
@@ -29,22 +29,37 @@ space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("hatter.png")
 space.addshape("sprite.gif")
+space.addshape("meteor2.gif")
 space.tracer(0)
 space.listen()
-space.onkey(balra,"Left")
-space.onkey(jobbra,"Right")
-space.onkey(fel,"Up")
-space.onkey(le,"Down")
+space.onkeypress(balra,"Left")
+space.onkeypress(jobbra,"Right")
+space.onkeypress(fel,"Up")
+space.onkeypress(le,"Down")
 
 
 urhajo = turtle.Turtle()
 urhajo.shape("sprite.gif")
 urhajo.penup()
 
+meteorjobb = turtle.Turtle()
+meteorjobb.shape("meteor2.gif")
+meteorjobb.penup()
+
+x = random.randint(130, 380)
+y = random.randint(-280, 280)
+meteorjobb.goto(x, y)
+while meteorjobb.xcor()>-400:
+    space.update()
+    time.sleep(0.2)
+    meteormozgas = meteorjobb.xcor()
+    meteormozgas -= 10
+    meteorjobb.setx(meteormozgas)
+
 while True:
 
     space.update()
-    time.sleep(0.2)
+    time.sleep(0.2)   
 
     if urhajo.ycor()>300:
         urhajo.sety(-300)
